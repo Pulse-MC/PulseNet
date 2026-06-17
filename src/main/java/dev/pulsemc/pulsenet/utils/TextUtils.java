f65faa13210ca193e2686d9a640c4bfb9d73393c
borisshoes <boris@borisshoes.net>
2026-06-16 21:37:13 -0400

    26.2

--- src/main/java/dev/pulsemc/pulsenet/utils/TextUtils.java
+++ src/main/java/dev/pulsemc/pulsenet/utils/TextUtils.java
@@ -2,60 +2,13 @@ package dev.pulsemc.pulsenet.utils;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.*;
-import net.minecraft.network.chat.contents.PlainTextContents;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.util.Tuple;
-import net.minecraft.world.level.Level;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Locale;
-import java.util.function.UnaryOperator;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class TextUtils {
    
    public static String camelToSnake(String str){
       return str.replaceAll("([a-z])([A-Z]+)", "$1_$2").toLowerCase(Locale.ROOT);
-   }
-   
-   public static final ArrayList<Tuple<ChatFormatting,Integer>> COLOR_MAP = new ArrayList<>(Arrays.asList(
-         new Tuple<>(ChatFormatting.BLACK,0x000000),
-         new Tuple<>(ChatFormatting.DARK_BLUE,0x0000AA),
-         new Tuple<>(ChatFormatting.DARK_GREEN,0x00AA00),
-         new Tuple<>(ChatFormatting.DARK_AQUA,0x00AAAA),
-         new Tuple<>(ChatFormatting.DARK_RED,0xAA0000),
-         new Tuple<>(ChatFormatting.DARK_PURPLE,0xAA00AA),
-         new Tuple<>(ChatFormatting.GOLD,0xFFAA00),
-         new Tuple<>(ChatFormatting.GRAY,0xAAAAAA),
-         new Tuple<>(ChatFormatting.DARK_GRAY,0x555555),
-         new Tuple<>(ChatFormatting.BLUE,0x5555FF),
-         new Tuple<>(ChatFormatting.GREEN,0x55FF55),
-         new Tuple<>(ChatFormatting.AQUA,0x55FFFF),
-         new Tuple<>(ChatFormatting.RED,0xFF5555),
-         new Tuple<>(ChatFormatting.LIGHT_PURPLE,0xFF55FF),
-         new Tuple<>(ChatFormatting.YELLOW,0xFFFF55),
-         new Tuple<>(ChatFormatting.WHITE,0xFFFFFF)
-   ));
-   
-   public static ChatFormatting getClosestFormatting(int colorRGB){
-      ChatFormatting closest = ChatFormatting.WHITE;
-      double cDist = Integer.MAX_VALUE;
-      for(Tuple<ChatFormatting, Integer> pair : COLOR_MAP){
-         int repColor = pair.getB();
-         double rDist = (((repColor>>16)&0xFF)-((colorRGB>>16)&0xFF))*0.30;
-         double gDist = (((repColor>>8)&0xFF)-((colorRGB>>8)&0xFF))*0.59;
-         double bDist = ((repColor&0xFF)-(colorRGB&0xFF))*0.11;
-         double dist = rDist*rDist + gDist*gDist + bDist*bDist;
-         if(dist < cDist){
-            cDist = dist;
-            closest = pair.getA();
-         }
-      }
-      return closest;
    }
    
    public static String intToRoman(int num){
